dc6ff245c9db9faeee7b2ce818a7c1862825507d
Hua <nickibunny@gmail.com>
2019-02-25 05:19:11 +0000

    Hello, world, you are at the polls index

--- videoshare/urls.py
+++ videoshare/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     #url(r'^$', views.login, name='login'),
     url(r'^login/$',views.auth_login,name = 'auth_login'),
-    url(r'^regist/$',views.auth_register,name = 'register'),
+    #url(r'^regist/$',views.auth_register,name = 'register'),
     #url(r'^index/$',views.index,name = 'index'),
-    url(r'^logout/$',views.auth_logout,name = 'logout'),
-    url(r'^videoshare/$', views.listvideo, name = 'list_video'),
+    #url(r'^logout/$',views.auth_logout,name = 'logout'),
+    #url(r'^videoshare/$', views.listvideo, name = 'list_video'),
 ]
